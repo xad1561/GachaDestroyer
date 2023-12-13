679d8d5a6ee26604ea73195441f2877cb678b855
xad1561 <70078325+xad1561@users.noreply.github.com>
2023-12-13 03:09:24 -0800

    Add standalone install directories, More Domains

--- GachaDestroyer/Program.cs
+++ GachaDestroyer/Program.cs
@@ -1,6 +1,9 @@
 ﻿//This program claims to be a set of tools for Genshin Impact/other gacha games but instead deletes the game(s) in question and blocks domains related to them to prevent the games from being reinstalled or played again
 //Currently only targets Genshin Impact, Honkai Star Rail, and Honkai Impact 3rd, which is only the tip of the iceberg
 //Still working on the "claims to be a set of tools" part
+
+//Spaghetti code prepared by xad1561
+
 /*
  * TODO:
  * Get domains and delete Honkai 3 as well
@@ -23,16 +26,21 @@ namespace GachaDestroyer
             bool blockDomains = false;
 
             //Default Directory Paths, change these as necessary
-            string GenshinDir = @"C:\Program Files\Epic Games\GenshinImpact";
-            string StarRailDir = @"C:\Program Files\Epic Games\HonkaiStarRail";
+            //Unrelated but the process of finding these felt heretical to me
+            string GenshinDirEpic = @"C:\Program Files\Epic Games\GenshinImpact";
+            string GenshinDir = @"C:\Program Files\Genshin Impact";
+            string StarRailDirEpic = @"C:\Program Files\Epic Games\HonkaiStarRail";
+            string StarRailDir = @"C:\Program Files\Star Rail";
             string Honkai3DirSteam = @"C:\Program Files (x86)\Steam\steamapps\common\HonkaiImpact3rd";
             string Honkai3DirEpic = @"C:\Program Files\Epic Games\HonkaiImpact3rd";
+            string Honkai3Dir = @"C:\Program Files\Honkai Impact 3rd";
 
             //The strings of the domains to block. 
             string[] domainsToBlock =
             {
                 "genshin.hoyoverse.com",
                 "hk4e-launcher.hoyoverse.com",
+                "hk4e-launcher-static.hoyoverse.com",
                 "sg-public-api-static.hoyoverse.com",
                 "launcher-webstatic.hoyoverse.com",
                 "sdk.hoyoverse.com",
@@ -41,11 +49,16 @@ namespace GachaDestroyer
                 "minor-api-os.hoyoverse.com",
                 "starrail.hoyoverse.com",
                 "abtest-api-data-sg.hoyoverse.com",
+                "log-upload-os.hoyoverse.com",
                 "hkrpg-launcher-static.hoyoverse.com",
                 "api-global-takumi-static.mihoyo.com",
                 "sdk-os-static.hoyoverse.com",
                 "hoyoverse.com",
-                "mihoyo.com"
+                "mihoyo.com",
+                "hsr.hoyoverse.com",
+                "honkaiimpact3.mihoyo.com",
+                "hoyolab.com",
+                "zenless.hoyoverse.com" //Future Proofing
             };
 
             //Check if the --DELETE/--BLOCK argument is given before deleting the files/blocking domains; Idiot Proofing. I sure hope there isn't a file type that lets you write a script, to execute stuff in a batch perhaps, where the arguments could be passed into it from there.
@@ -85,51 +98,79 @@ namespace GachaDestroyer
             //Collide, the function that deletes Genshin.
             void Collide()
             {
+                //Epic Games
+                if (Directory.Exists(GenshinDirEpic))
+                {
+                    Console.WriteLine("Genshin Impact Detected on Epic Games");
+                    try
+                    {
+                        if (actuallyDeleteFiles) { Directory.Delete(GenshinDirEpic, true); }
+                    }
+                    catch { }
+                    /*
+                    if (dateTime.Contains("8/28/"))
+                    {
+                        Console.WriteLine("Unhappy Birthday Genshin!");
+                    }*/
+                }
+                else
+                {
+                    Console.WriteLine("Genshin Impact not detected on Epic Games");
+                }
+
+                //Standalone
                 if (Directory.Exists(GenshinDir))
                 {
-                    Console.WriteLine("Genshin Impact Detected");
+                    Console.WriteLine("Standalone Genshin Impact Detected");
                     try
                     {
                         if (actuallyDeleteFiles) { Directory.Delete(GenshinDir, true); }
                     }
                     catch { }
-
-                    if (dateTime.Contains("8/28/"))
-                    {
-                        Console.WriteLine("Unhappy Birthday Genshin!");
-                    }
                 }
                 else
                 {
-                    Console.WriteLine("Genshin not detected");
+                    Console.WriteLine("Standalone Genshin Impact not detected");
                 }
             }
 
             //Derail, for removing Star Rail
             void Derail()
             {
-                if (Directory.Exists(StarRailDir))
+                //Epic Games
+                if (Directory.Exists(StarRailDirEpic))
                 {
-                    Console.WriteLine("Star Rail Detected");
+                    Console.WriteLine("Honkai Star Rail Detected");
                     try
                     {
-                        if (actuallyDeleteFiles) { Directory.Delete(StarRailDir, true); }
+                        if (actuallyDeleteFiles) { Directory.Delete(StarRailDirEpic, true); }
                     } catch { }
-
-                    if (dateTime.Contains("4/25/") | dateTime.Contains("3/07/"))
-                    {
-                        Console.WriteLine("I don't know what to put except that you are probably a gambling addict and a simp.");
-                    }
                 }
                 else
                 {
                     Console.WriteLine("Honkai Star Rail not detected");
+                }
+
+                //Standalone
+                if (Directory.Exists(StarRailDir))
+                {
+                    Console.WriteLine("Standalone Honkai Star Rail Detected");
+                    try
+                    {
+                        if (actuallyDeleteFiles) { Directory.Delete(StarRailDir, true); }
+                    }
+                    catch { }
+                }
+                else
+                {
+                    Console.WriteLine("Standalone Honkai Star Rail not detected");
                 }
             }
 
             //Repurcussion, for deleting Honkai 3. The reason it is named that is because it is apparently similar in meaning to impact according to the thesaurus. 
             void Repurcussion()
             {
+                //Epic Games
                 if (Directory.Exists(Honkai3DirEpic))
                 {
                     Console.WriteLine("Honkai 3 Detected on Epic Games");
@@ -143,6 +184,7 @@ namespace GachaDestroyer
                     Console.WriteLine("Honkai 3 not detected on Epic Games");
                 }
 
+                //Steam
                 if (Directory.Exists(Honkai3DirSteam))
                 {
                     Console.WriteLine("Honkai 3 Detected on Steam");
@@ -154,6 +196,21 @@ namespace GachaDestroyer
                 else
                 {
                     Console.WriteLine("Honkai 3 not detected on Steam");
+                }
+
+                //Standalone
+                if (Directory.Exists(Honkai3Dir))
+                {
+                    Console.WriteLine("Standalone Honkai 3 Detected on Steam");
+                    try
+                    {
+                        if (actuallyDeleteFiles) { Directory.Delete(Honkai3Dir, true); }
+                    }
+                    catch { }
+                }
+                else
+                {
+                    Console.WriteLine("Standalone Honkai 3 not detected");
                 }
             }
 
